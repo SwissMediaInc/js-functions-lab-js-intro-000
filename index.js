2c5b7ba71a7fb35c7f491e284745f73dd66656db
Jay Friedmann <jay@swissmediainc.net>
2019-03-08 02:16:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
 
-functions happyHolidaysTo(holdiay, name){
+function happyHolidaysTo(holdiay, name){
   return `Happy ${holiday}, ${name}!`
 }
